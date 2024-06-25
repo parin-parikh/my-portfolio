76edead048b979711f1e4b020cef74cf065714ed
parin-parikh <parinparikh1@gmail.com>
2024-06-25 17:26:08 -0400

    update resume button

--- app/(home)/components/ResumeButton.tsx
+++ app/(home)/components/ResumeButton.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 export default function ResumeButton() {
   return (
     <div>   
-        <a href="/Parin Parikh Resume.pdf" download className=" bg-green-500 hover:bg-indigo-400 text-white font-bold py-2 px-4 rounded cursor-pointer">
-        Download Resume 📄
+        <a href="https://www.parinresume.xyz" className=" bg-green-500 hover:bg-indigo-400 text-white font-bold py-2 px-4 rounded cursor-pointer">
+        View Resume 📄
         </a>
     </div>
     
